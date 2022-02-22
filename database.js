d5d3eb95ef22b18d87b99d122aae1cc83d339914
Eri Schon <erischon@gmail.com>
2022-02-22 15:11:55 +0100

    initialisation

---
+++ database.js
@@ -0,0 +1,22 @@
+const database = {
+    users: [
+        {
+            id: "123",
+            name: "john",
+            email: "john@gmail.com",
+            password: "cookies",
+            entries: 0,
+            joined: new Date()  
+        },
+        {
+            id: "124",
+            name: "eri",
+            email: "eri@gmail.com",
+            password: "bananas",
+            entries: 0,
+            joined: new Date()  
+        }
+    ]
+}
+
+// export default database
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const bcrypt = require('bcrypt-nodejs') 
+const cors = require('cors')
+const knex = require("knex")
+require('dotenv').config();
+
+const register = require("./controllers/register")
+const signin = require("./controllers/signin")
+const profile = require("./controllers/profile")
+const image = require("./controllers/image")
+
+const db = knex({
+    client: "pg",
+    connection: {
+        host: "localhost",
+        port: 5433,
+        user: "admin",
+        password: "secret",
+        database: "smart-brain"
+    }
+})
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => { res.send("Success"); })
+
+app.post("/signin", (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put("/image", (req, res) => { image.handleImage(req, res, db) })
+app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) })
+
+const PORT = process.env.PORT
+app.listen(PORT, ()=> {
+    console.log(`server is listening on port ${PORT}`);
+})
